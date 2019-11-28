703f28756849b2851ab5b2b82660ad6fc8531c05
Gayuru <ggayurugunawardana@gmail.com>
2019-11-28 20:28:56 +1100

    Project initialisation
    The project has been initialised with a blank Xcode project along with the pods needed as well as a blank python project.

---
+++ src/frontend_swift/OneBank/OneBank/ViewModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneBank
+//
+//  Created by Gayuru Gunawardana on 28/11/19.
+//  Copyright © 2019 iSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
