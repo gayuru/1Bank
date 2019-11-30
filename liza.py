734d790095632da879dd85b7bfaef71b3dfe23a0
Elizabeth <lizatawaf@gmail.com>
2019-11-30 14:48:22 +1100

    Update liza.py

--- liza.py
+++ liza.py
@@ -18,7 +18,7 @@ class Todo(db.Model):
         return '<Task  %r>' % self.id
 
 
-@app.route('/', methods=['POST', 'GIT'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
