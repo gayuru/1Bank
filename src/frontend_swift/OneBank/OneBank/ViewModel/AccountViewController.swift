4f0b72454360b65d60257416d3e40ab07ca2eeaf
Gayuru <ggayurugunawardana@gmail.com>
2019-12-01 07:15:19 +1100

    changes to bank

--- src/frontend_swift/OneBank/OneBank/ViewModel/AccountViewController.swift
+++ src/frontend_swift/OneBank/OneBank/ViewModel/AccountViewController.swift
@@ -12,12 +12,15 @@ class AccountViewController: UIViewController {
 
     @IBOutlet weak var bankView: UIView!
     var currentAccount:String!
+    @IBOutlet weak var accountsCardCollection: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
         bankView.layer.cornerRadius = 50
         bankView.layer.maskedCorners = [.layerMinXMaxYCorner]
         
+        accountsCardCollection.delegate = self
+        accountsCardCollection.dataSource = self
         switch currentAccount {
         case "CBA":
             self.bankView.backgroundColor = UIColor(red: 255, green: 204, blue: 0)
@@ -29,4 +32,21 @@ class AccountViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+}
+
+extension AccountViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardCell", for: indexPath) as! AccountsCardCollectionViewCell
+        cell.balanceLabel.text = "14.000"
+        cell.cardNumber.text! + "**** **** **** 1422)"
+        cell.cardTypeLabel.text = "VISA"
+        
+        return cell
+    }
+    
+    
 }
