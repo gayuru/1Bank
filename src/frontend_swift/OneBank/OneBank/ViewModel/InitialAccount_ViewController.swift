0d1ae832a99e51556c0abd64ec8bea86e30ef3f6
Sogyal <sogyals429@gmail.com>
2019-12-01 01:17:46 +1100

    Working on Cleaning Home

--- src/frontend_swift/OneBank/OneBank/ViewModel/InitialAccount_ViewController.swift
+++ src/frontend_swift/OneBank/OneBank/ViewModel/InitialAccount_ViewController.swift
@@ -14,6 +14,7 @@ class InitialAccount_ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
         existingBankBtn.layer.borderWidth = 1.0
         existingBankBtn.layer.borderColor = UIColor(red:0.24, green:0.19, blue:0.75, alpha:1.0).cgColor
         // Do any additional setup after loading the view.
