abe91bec637c137c07f4341ca611864671dd3cee
nchong128 <nchong128@gmail.com>
2019-11-30 16:32:42 +1100

    add models

--- src/backend_python/application.py
+++ src/backend_python/application.py
@@ -14,6 +14,44 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   firstName = db.Column(db.String(200), unique=False, nullable=True)
   lastName = db.Column(db.String(200), unique=False, nullable=True)
+  password = db.Column(db.String(200), unique=False,nullable=True)
+  bankAccounts = db.relationship("BankAccount", backref='user')
+
+  def __repr__(self):
+    return '<User  %r>' % self.id
+
+class BankAccount(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  accountName = db.Column(db.String(200), unique=False, nullable=True)
+  bankName = db.Column(db.String(200), unique=False, nullable=True)
+
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  cards = db.relationship('Card', backref='bankAccount', lazy = True)
+  transactions = db.relationship('Transaction', backref='bankAccount', lazy = True)
+
+  def __repr__(self):
+    return '<Bank Account  %r>' % self.id
+
+class Transaction(db.Model):
+  transId = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.Date)
+  amount = db.Column(db.Float)
+
+  bankaccount_id = db.Column(db.Integer, db.ForeignKey('bankAccount.id'), nullable = False)
+  transactionGroup = db.relationship('TransactionGroup', backref='transaction', lazy = True)
+
+class TransactionGroup(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(200), unique= False)
+
+  transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), nullable = False)
+
+class Card(db.Model):
+  number = db.Column(db.String(200), primary_key=True)
+  type = db.Column(db.String(200), unique= False)
+  expiryDate = db.Column(db.Date, unique= False)
+
+  bankaccount_id = db.Column(db.Integer, db.ForeignKey('bankAccount.id'), nullable = False)
 
 db.create_all()
 print("db created", flush=True)
